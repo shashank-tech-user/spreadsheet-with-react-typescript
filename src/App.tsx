a3819594a436e2bafafb1fedfb878a320351db8f
shashank-tech-user <113688593+shashank-tech-user@users.noreply.github.com>
2023-01-24 12:53:12 +0530

    Changed license key (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -6,44 +6,54 @@ import './App.css';
 import "jspreadsheet/dist/jspreadsheet.css";
 import "jsuites/dist/jsuites.css";
 
+// https://jspreadsheet.com/v9/docs/cells
+
 function App() {
   const [dimensions, setDimenstions] = useState({
-    rows: 6,
+    rows: 2,
     cols: 10
   })
   const jssRef: any = useRef(null);
   const spreadsheet: any = useRef(null);
   jspreadsheet.setLicense(
-    'ZTYyMTQ4MjY3MTAwYjFhOTBmMjIwY2UwYTdjMWIyZTczYWQ4MWFlYzFkYzZlNDYzZWY0ZTExOWE0Mjg4NmIyNTI1ZWU2ZmVmZDgwMjU4NDVhNGYxY2U4ZjYzZGZjNGU3OGRjNjA3NjdkNDVmZTUyMjkwOTlkM2ViY2M2NTQ0MjksZXlKdVlXMWxJam9pU25Od2NtVmhaSE5vWldWMElpd2laR0YwWlNJNk1UWTVOVGMyT1RJd01Dd2laRzl0WVdsdUlqcGJJbXB6YUdWc2JDNXVaWFFpTENKcWMzQnlaV0ZrYzJobFpYUXVZMjl0SWl3aVkzTmlMbUZ3Y0NJc0luTmhiM0p2WTJzdVkyOXRJaXdpZFdVdVkyOXRMbUp5SWl3aWRXNXBkR1ZrTG1Wa2RXTmhkR2x2YmlJc0ltTnZaR1Z6WVc1a1ltOTRMbWx2SWl3aWJHOWpZV3hvYjNOMElsMHNJbkJzWVc0aU9pSXpNU0lzSW5OamIzQmxJanBiSW5ZM0lpd2lkamdpTENKMk9TSXNJbVp2Y20xeklpd2labTl5YlhWc1lTSXNJbkpsYm1SbGNpSXNJbkJoY25ObGNpSXNJbWx0Y0c5eWRHVnlJaXdpZG1Gc2FXUmhkR2x2Ym5NaUxDSmpiMjF0Wlc1MGN5SXNJbk5sWVhKamFDSXNJbU5vWVhKMGN5SXNJbU5zYjNWa0lpd2lZbUZ5SWl3aWNISnBiblFpTENKdFlYTnJJaXdpYzJobFpYUnpJbDE5'
+    'YjdkYTU5OWUxOGM2MzU4MTkyZmE3N2NiZTE5ZjRkMDAxNGZjMWI5Nzg4NWM2NjdlY2U5NmI5Y2I2OTc1YzE5YjM5NzQ1MjkyYzFmODE0MzczNjY4NmQxYjc5M2U3ZjE5NDhjMTlkMDViYTAyMWI2MjAzNmQ2OTM1MmE0YzlhYjEsZXlKdVlXMWxJam9pVTJoaGMyaGhibXNnVUdGdVkyaGhiQ0lzSW1SaGRHVWlPakUyTnpjeE9UWTRNREFzSW1SdmJXRnBiaUk2V3lJaUxDSnNiMk5oYkdodmMzUWlYU3dpY0d4aGJpSTZNQ3dpYzJOdmNHVWlPbHNpZGpjaUxDSjJPQ0lzSW5ZNUlsMTk='
   );
 
   jspreadsheet.setExtensions({ render });
 
+  let options: any = {
+    tabs: true,
+    worksheets: [{
+      worksheetName: 'Sales',
+      minDimensions: [dimensions.cols, dimensions.rows],
+      data: [
+        ['2023-01-23', 0, 20, 0, 0, 0, 0, 0, 0],
+        ['2023-01-23', 0, 300, 310, 100, 50, 20, 30, 0],
+        ['2023-01-24', 0, 300, 310, 100, 50, 20, 30, 0]
+      ],
+      columns: [
+        { type: "calendar", title: "Date" },
+        { type: "numeric", title: "Total Sales" },
+        { type: "numeric", title: "Food", },
+        { type: "numeric", title: "Liquor", },
+        { type: "numeric", title: "Beer", },
+        { type: "numeric", title: "Wine", },
+        { type: "numeric", title: "N/A Bev", },
+        { type: "numeric", title: "MDSE", },
+        { type: "numeric", title: "GIFT CERT", },
+      ]
+    }, {
+      worksheetName: 'Comps',
+      minDimensions: [dimensions.cols, dimensions.rows],
+      data: [[100, 200], [10, 20, 30]]
+    }]
+  }
+
   useEffect(() => {
     if (!jssRef.current.jspreadsheet) {
-      spreadsheet.current = jspreadsheet(jssRef.current, {
-        tabs: true,
-        worksheets: [{
-          minDimensions: [dimensions.rows, dimensions.cols],
-          data: [['2023-01-23', 0, 20, 0, 0, 0, 0, 0, 0], ['2023-01-23', 0, 300, 310, 100, 50, 20, 30, 0]],
-          columns: [
-            { type: "calendar", title: "Date", },
-            { type: "numeric", title: "Total Sales", },
-            { type: "numeric", title: "Food", },
-            { type: "numeric", title: "Liquor", },
-            { type: "numeric", title: "Beer", },
-            { type: "numeric", title: "Wine", },
-            { type: "numeric", title: "N/A Bev", },
-            { type: "numeric", title: "MDSE", },
-            { type: "numeric", title: "GIFT CERT", },
-          ]
-        }, {
-          minDimensions: [dimensions.rows, dimensions.cols],
-          data: [[100, 200], [10, 20, 30]]
-        }]
-      })
+      spreadsheet.current = jspreadsheet(jssRef.current, options);
     }
-  }, []);
+  }, [options]);
 
   return (
     <div className="App">
@@ -58,7 +68,7 @@ function App() {
       <h1>Vite + React</h1>
       <hr />
       <div>
-        <div ref={jssRef}></div>
+        <div ref={jssRef} />
       </div>
     </div>
   )
