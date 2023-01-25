07653e1b8af0b336c4b2890ca1a13feb21e95c87
shashank-tech-user <113688593+shashank-tech-user@users.noreply.github.com>
2023-01-25 18:02:47 +0530

    Development (#5)
    * Changed license key
    
    * store all the data with state variables.
    
    * Added validation for cell values
    
    * remove validations
    
    * Remove validations
    
    * Added validation string
    
    * Remove validation from main file (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -107,8 +107,8 @@ function App() {
     console.log('jssRef.current => ', jssRef.current);
     // jssRef.current.setReadOnlyCell(0, 0, true);
   }
-  
-  jspreadsheet.setExtensions({ validations, render });
+
+  jspreadsheet.setExtensions({ render });
 
   const handleChangeValues = (worksheet: any, cell: any, col: any, row: any, value: any) => {
     let sheetName = worksheet.getWorksheetName();
@@ -166,14 +166,6 @@ function App() {
           setReadOnlyCell();
         },
         worksheets: allData,
-        validations: [{
-          range: 'Sales!C1:C6',
-          action: "warning",
-          criteria: ">=",
-          type: "number",
-          allowBlank: false,
-          value: [0],
-        }],
         onchange: handleChangeValues,
         oncomments: function () {
           console.log('oncomments', arguments);
