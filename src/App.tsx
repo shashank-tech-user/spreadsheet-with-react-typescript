e8194950039bf6a4ced061699b05f060db72fda7
Shashank <shashank.panchal@technostacks.com>
2023-01-27 18:49:40 +0530

    Create sheet data constant variable. Added styling to sheet. Added background color to specific cell.

--- src/App.tsx
+++ src/App.tsx
@@ -5,106 +5,21 @@ import { render } from "@jspreadsheet/render";
 import './App.css';
 import "jspreadsheet/dist/jspreadsheet.css";
 import "jsuites/dist/jsuites.css";
+import { ColsIndex, data, RowsIndex } from "./constant";
 
 // https://jspreadsheet.com/v9/docs/cells
 
 function App() {
-  let locale = 'en-US';
-  let currencyMast = {
-    style: 'currency',
-    currency: 'USD'
-  }
-  let dateMask = 'dd/mm/yyyy';
   const jssRef: any = useRef(null);
   const spreadsheet: any = useRef(null);
   const [dimensions, setDimenstions] = useState({
     rows: 2,
     cols: 10
   })
-  const data = [{
-    worksheetName: 'Sales',
-    minDimensions: [dimensions.cols, dimensions.rows],
-    data: [
-      ['2023-01-23', '=SUM(C1:I1)', 20, 0, 0, 0, 0, 0, 0],
-      ['2023-01-23', '=SUM(C2:I2)', 300, 310, 100, 50, 20, 30, 0],
-      ['2023-01-24', '=SUM(C3:I3)', 300, 310, 100, 50, 20, 30, 0],
-      ['2023-01-24', '=SUM(C4:I4)', 300, 310, 100, 50, 20, 30, 0],
-      ['2023-01-24', '=SUM(C5:I5)', 300, 310, 100, 50, 20, 30, 0],
-      ['2023-01-24', '=SUM(C6:I6)', 300, 310, 100, 50, 20, 30, 0],
-      ['2023-01-24', '=SUM(C7:I6)', 300, 310, 100, 50, 20, 30, 0],
-      ['2023-01-24', '=SUM(B1:B7)', '=SUM(C1:C7)', '=SUM(D1:D7)', '=SUM(E1:E7)', '=SUM(F1:F7)', '=SUM(G1:G7)', '=SUM(H1:H7)', '=SUM(I1:I7)'],
-      ['2023-01-24', '=SUM(C3:I3)', 300, 310, 100, 50, 20, 30, 0]
-    ],
-    columns: [
-      {
-        type: "calendar",
-        mask: dateMask,
-        title: "Date"
-      },
-      {
-        type: "number",
-        title: "Total Sales",
-        locale: locale,
-        options: currencyMast,
-        readOnly: true
-      },
-      {
-        type: "number",
-        title: "Food",
-        locale: locale,
-        options: currencyMast
-      },
-      {
-        type: "number",
-        title: "Liquor",
-        locale: locale,
-        options: currencyMast
-      },
-      {
-        type: "number",
-        title: "Beer",
-        locale: locale,
-        options: currencyMast
-      },
-      {
-        type: "number",
-        title: "Wine",
-        locale: locale,
-        options: currencyMast
-      },
-      {
-        type: "number",
-        title: "N/A Bev",
-        locale: locale,
-        options: currencyMast
-      },
-      {
-        type: "number",
-        title: "MDSE",
-        locale: locale,
-        options: currencyMast
-      },
-      {
-        type: "number",
-        title: "GIFT CERT",
-        locale: locale,
-        options: currencyMast
-      }
-    ]
-  }, {
-    worksheetName: 'Comps',
-    minDimensions: [dimensions.cols, dimensions.rows],
-    data: [[90, 60, 80, '=SUM(A1:C1,Sales!B1)'], [800, 900, 7800, '=SUM(A2:C2,Sales!B2)']]
-  }]
   const [allData, setAllData] = useState<any[]>(data);
   jspreadsheet.setLicense(
     'YjdkYTU5OWUxOGM2MzU4MTkyZmE3N2NiZTE5ZjRkMDAxNGZjMWI5Nzg4NWM2NjdlY2U5NmI5Y2I2OTc1YzE5YjM5NzQ1MjkyYzFmODE0MzczNjY4NmQxYjc5M2U3ZjE5NDhjMTlkMDViYTAyMWI2MjAzNmQ2OTM1MmE0YzlhYjEsZXlKdVlXMWxJam9pVTJoaGMyaGhibXNnVUdGdVkyaGhiQ0lzSW1SaGRHVWlPakUyTnpjeE9UWTRNREFzSW1SdmJXRnBiaUk2V3lJaUxDSnNiMk5oYkdodmMzUWlYU3dpY0d4aGJpSTZNQ3dpYzJOdmNHVWlPbHNpZGpjaUxDSjJPQ0lzSW5ZNUlsMTk='
-  );
-
-  const setReadOnlyCell = () => {
-    console.log('jssRef.current => ', jssRef.current);
-    // jssRef.current.setReadOnlyCell(0, 0, true);
-  }
+  )
 
   jspreadsheet.setExtensions({ render });
 
@@ -121,32 +36,10 @@ function App() {
     setAllData(newArr);
   }
 
-  let options: any = {
-    tabs: true,
-    worksheets: [{
-      worksheetName: 'Sales',
-      minDimensions: [dimensions.cols, dimensions.rows],
-      data: [
-        ['2023-01-23', 0, 20, 0, 0, 0, 0, 0, 0],
-        ['2023-01-23', 0, 300, 310, 100, 50, 20, 30, 0],
-        ['2023-01-24', 0, 300, 310, 100, 50, 20, 30, 0]
-      ],
-      columns: [
-        { type: "calendar", title: "Date" },
-        { type: "numeric", title: "Total Sales" },
-        { type: "numeric", title: "Food", },
-        { type: "numeric", title: "Liquor", },
-        { type: "numeric", title: "Beer", },
-        { type: "numeric", title: "Wine", },
-        { type: "numeric", title: "N/A Bev", },
-        { type: "numeric", title: "MDSE", },
-        { type: "numeric", title: "GIFT CERT", },
-      ]
-    }, {
-      worksheetName: 'Comps',
-      minDimensions: [dimensions.cols, dimensions.rows],
-      data: [[100, 200], [10, 20, 30]]
-    }]
+  const handleReadOnlyCells = (worksheet: any, cell: any, col: any, row: any, value: any) => {
+    if (RowsIndex.includes(row) && ColsIndex.includes(col))
+      cell.classList.add('readonly');
+
   }
 
   useEffect(() => {
@@ -161,29 +54,21 @@ function App() {
         loadingSpin: true,
         onload: function (spreadsheet) {
           spreadsheet.openWorksheet(0);
-          setReadOnlyCell();
         },
         worksheets: allData,
         onchange: handleChangeValues,
+        updateTable: handleReadOnlyCells,
         oncomments: function () {
           console.log('oncomments', arguments);
         },
         license: 'Njg1YTU0ZTYwNDk1YmI5ZWViNWNiYmJhZWMwNjRlM2RiODA1Y2YwZTYzMGJjNjllZWFjODhjYjNmMzIyZWVlMjRmNzhmNzVkMmRlZWMzMDMyMTczMWQyODA1ZmIzYzM2ZWExYWVhOGUwYzUzMTVkNjI3MmJjMDVlMjA2OWVhNTMsZXlKdVlXMWxJam9pVTJoaGMyaGhibXNnVUdGdVkyaGhiQ0lzSW1SaGRHVWlPakUyTnpjeU9ETXlNREFzSW1SdmJXRnBiaUk2V3lKemNISmxZV1J6YUdWbGRDMTNhWFJvTFhKbFlXTjBMWFI1Y0dWelkzSnBjSFF1ZG1WeVkyVnNMbUZ3Y0NJc0lteHZZMkZzYUc5emRDSmRMQ0p3YkdGdUlqb3dMQ0p6WTI5d1pTSTZXeUoyTnlJc0luWTRJaXdpZGpraUxDSm1iM0p0ZFd4aElpd2labTl5YlhNaUxDSnlaVzVrWlhJaUxDSndZWEp6WlhJaUxDSnBiWEJ2Y25SbGNpSXNJblpoYkdsa1lYUnBiMjV6SWl3aVkyOXRiV1Z1ZEhNaUxDSnpaV0Z5WTJnaUxDSmphR0Z5ZEhNaVhYMD0=',
       });
     }
-  }, [options]);
+  }, [allData]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h3>The BrassTap</h3>
       <hr />
       <div>
         <div ref={jssRef} />
